4329ca515ad0ad77bffa35e06b35a9ecca22378e
Yevhenii Chernenko <chernenko.yevheniy@gmail.com>
2024-12-12 13:26:15 +0100

    Templates

--- actor_app/views.py
+++ actor_app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 index = Blueprint('list', __name__)
 actor = Blueprint('actor', __name__, url_prefix='/actor')
@@ -8,12 +8,12 @@ movie = Blueprint('movie', __name__, url_prefix='/movie')
 @index.route('/')
 @index.route('/actor/')
 def index_page():
-    return "Main page with list of actors"
+    return render_template('index.html')
 
-@actor.route('/:id')
+@actor.route('/<id>')
 def actor_page(id):
-    return "Page with actor {}".format(id)
+    return render_template('actor.html', id=id)
 
 @movie.route('/')
 def movie_page():
-    return "Movie statistics here!"
+    return render_template('movie.html')
